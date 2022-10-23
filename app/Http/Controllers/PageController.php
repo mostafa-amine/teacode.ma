0ae96602482d9244ee2d6ad272c1c0b68449270d
Driss <drissboumlik.dev@gmail.com>
2022-10-23 22:58:13 +0100

    Refactor page controller (WIP)

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contributor;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -21,17 +22,28 @@ class PageController extends Controller
     public function contributors(Request $request)
     {
         $data = new \stdClass;
-        $data->contributors = json_decode(\File::get(base_path() . '/database/data/contributors.json'));
+        $data->contributors = Contributor::all();
         $data->title = 'TeaCode | Contributors';
-
-        $data->contributors = collect($data->contributors)
-                                    ->map(function ($contributor, $key) {
-                                        // $contributor->color =  getColorRole($contributor->role);
-                                        $contributor->image = getContributorImage($contributor, $key);
-                                        $contributor->badge = getContributorBadge($contributor);
-                                        return $contributor;
-                                    })->shuffle()->values();
-
+//        $db_data = [];
+//        try {
+//            \DB::beginTransaction();
+//            Contributor::truncate();
+//            $data->contributors = collect($data->contributors)
+//                                        ->map(function ($contributor, $key) use (&$db_data) {
+//                                            // $contributor->color =  getColorRole($contributor->role);
+//                                            $contributor->image = getContributorImage($contributor, $key);
+//                                            $contributor->badge = getContributorBadge($contributor->role);
+//                                            $db_data[] = (array)$contributor;
+//                                            return $contributor;
+//                                        }); //->shuffle()->values();
+//
+//            Contributor::insert($db_data);
+//            \DB::commit();
+//        } catch (\Throwable $th) {
+//            \DB::rollback();
+//            dd($db_data);
+//            throw $th;
+//        }
         return view('pages.contributors', ['data' => $data]);
     }
 
@@ -55,6 +67,32 @@ class PageController extends Controller
         $data = new \stdClass;
         $data->title = 'TeaCode | Terms of Use';
         return view('pages.terms-of-use', ['data' => $data]);
+    }
+
+    public function getAssets(Request $request, $type = null)
+    {
+        try {
+            $base_path = base_path();
+            $type ??= 'events';
+            try {
+                $path = base_path('../assets/shared/img/' . $type);
+                $_base_path = str_replace('base', '', $base_path);
+                $files = \File::files($path);
+            } catch (\Throwable $th) {
+                $path = public_path('assets/shared/img/' . $type);
+                $_base_path = $base_path . '\public\\';
+                $files = \File::files($path);
+            }
+            foreach ($files as $file) {
+                $file->webPath = str_replace($_base_path, '', $file->getRealPath());
+            }
+            $menu = json_decode(\File::get($base_path . '/database/data/admin/menu.json'));
+            $data = new \stdClass;
+            $data->title = 'TeaCode | Assets';
+            return view('pages.admin.assets', ['data' => $data, 'files' => $files, 'menu' => $menu]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     // public function rules(Request $request)

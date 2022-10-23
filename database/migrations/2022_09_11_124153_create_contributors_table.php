4e1ca4503188d6c33a1fdb11f1bc308c73632642
Driss <drissboumlik.dev@gmail.com>
2022-10-23 22:47:24 +0100

    Add contributor migration

---
+++ database/migrations/2022_09_11_124153_create_contributors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContributorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contributors', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('role');
+            $table->string('slug');
+            $table->string('image');
+            $table->string('badge');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contributors');
+    }
+}

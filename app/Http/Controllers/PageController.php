13cce30cecd09d9ac4a3034c57e6e965cabf6356
Driss <drissboumlik.dev@gmail.com>
2022-11-06 18:15:21 +0100

    Handle directories path in local and prod env

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -58,28 +58,37 @@ class PageController extends Controller
         try {
             $base_path = base_path();
             $type ??= 'events';
-            $directories = array_map(function ($directory) {
-                $dirs = explode('/', $directory);
-                return $dirs[count($dirs) - 1];
-            }, \Storage::disk('_public')->directories('assets/shared/img'));
-            try {
+            if (config('app.env') == 'production') {
+                $directories = array_filter(scandir(base_path('../assets/shared/img')), function($dir) {
+                    return $dir != '.' && $dir != '..';
+                });
+                if (!in_array($type, $directories)) {
+                    $type = 'events';
+                }
                 $path = base_path('../assets/shared/img/' . $type);
                 $_base_path = str_replace('base', '', $base_path);
-                $files = \File::files($path);
-            } catch (\Throwable $th) {
+            } else {
+                $directories = array_filter(scandir(public_path('/assets/shared/img')), function($dir) {
+                    return $dir != '.' && $dir != '..';
+                });
+                if (!in_array($type, $directories)) {
+                    $type = 'events';
+                }
                 $path = public_path('assets/shared/img/' . $type);
                 $_base_path = $base_path . '\public\\';
+            }
+            if (is_dir($path)) {
                 $files = \File::files($path);
+                foreach ($files as $file) {
+                    $file->webPath = str_replace($_base_path, '', $file->getRealPath());
+                }
+                $menu = json_decode(\File::get($base_path . '/database/data/admin/menu.json'));
+                $data = new \stdClass;
+                $data->title = 'TeaCode | Assets';
+                $data->type = $type;
+                $data->dirLinks = $directories;
+                return view('pages.admin.assets', ['data' => $data, 'files' => $files, 'menu' => $menu]);
             }
-            foreach ($files as $file) {
-                $file->webPath = str_replace($_base_path, '', $file->getRealPath());
-            }
-            $menu = json_decode(\File::get($base_path . '/database/data/admin/menu.json'));
-            $data = new \stdClass;
-            $data->title = 'TeaCode | Assets';
-            $data->type = $type;
-            $data->dirLinks = $directories;
-            return view('pages.admin.assets', ['data' => $data, 'files' => $files, 'menu' => $menu]);
         } catch (\Throwable $th) {
             throw $th;
         }

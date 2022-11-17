88b625f0dba46a648e53b67d1e3b24d241f39557
Driss <drissboumlik.dev@gmail.com>
2022-11-17 14:56:13 +0100

    Edit database params

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -10,15 +10,15 @@ class ToolController extends Controller
     {
         $dumpPath = database_path('dumps/dump.sql');
         \Spatie\DbDumper\Databases\MySql::create()
-            ->setDbName('teacode.ma')
-            ->setUserName('root')
-            ->setPassword('')
+            ->setDbName(env('DB_DATABASE'))
+            ->setUserName(env('DB_USERNAME'))
+            ->setPassword(env('DB_PASSWORD'))
             ->dumpToFile($dumpPath);
-            
-            return \Response::download($dumpPath, "dump.sql", [
-                'Content-Type' => 'application/sql',
-                // 'Content-Transfer-Encoding'=> 'binary',
-                // 'Accept-Ranges'=> 'bytes'
-            ]);
+
+        return \Response::download($dumpPath, "dump.sql", [
+            'Content-Type' => 'application/sql',
+            // 'Content-Transfer-Encoding'=> 'binary',
+            // 'Accept-Ranges'=> 'bytes'
+        ]);
     }
 }
